e063370ca01eb39f835f25bf8bb5daf7609cbee4
regispbr <reginaldo_uni9@hotmail.com>
2025-12-04 23:39:35 -0300

    Update widget.edit.php

--- modules/hostcounter/views/widget.edit.php
+++ modules/hostcounter/views/widget.edit.php
@@ -10,67 +10,56 @@
  * @var array $data
  */
 
-use Modules\HostCounter\Includes\WidgetForm;
-
 $form = new CWidgetFormView($data);
 
-// Host groups field
 if (array_key_exists('hostgroups', $data['fields'])) {
 	$form->addField(
 		new CWidgetFieldMultiSelectGroupView($data['fields']['hostgroups'])
 	);
 }
 
-// Hosts field
 if (array_key_exists('hosts', $data['fields'])) {
 	$form->addField(
 		new CWidgetFieldMultiSelectHostView($data['fields']['hosts'])
 	);
 }
 
-// Count problems field
 if (array_key_exists('count_problems', $data['fields'])) {
 	$form->addField(
 		new CWidgetFieldCheckBoxView($data['fields']['count_problems'])
 	);
 }
 
-// Count items field
 if (array_key_exists('count_items', $data['fields'])) {
 	$form->addField(
 		new CWidgetFieldCheckBoxView($data['fields']['count_items'])
 	);
 }
 
-// Count triggers field
 if (array_key_exists('count_triggers', $data['fields'])) {
 	$form->addField(
 		new CWidgetFieldCheckBoxView($data['fields']['count_triggers'])
 	);
 }
 
-// Count disabled hosts field
 if (array_key_exists('count_disabled', $data['fields'])) {
 	$form->addField(
 		new CWidgetFieldCheckBoxView($data['fields']['count_disabled'])
 	);
 }
 
-// Count maintenance hosts field
 if (array_key_exists('count_maintenance', $data['fields'])) {
 	$form->addField(
 		new CWidgetFieldCheckBoxView($data['fields']['count_maintenance'])
 	);
 }
 
-// Show suppressed problems field
 if (array_key_exists('show_suppressed', $data['fields'])) {
 	$form->addField(
 		new CWidgetFieldCheckBoxView($data['fields']['show_suppressed'])
 	);
 }
 
-// Custom icon field
 if (array_key_exists('custom_icon', $data['fields'])) {
 	$form->addField(
 		new CWidgetFieldTextBoxView($data['fields']['custom_icon'])
