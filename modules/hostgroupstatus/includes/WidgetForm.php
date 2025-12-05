5aa0be446fbee30fcd5292af921cf1878ef1f7d5
Dutervas <149430977+Dutervas@users.noreply.github.com>
2025-12-05 16:46:44 -0300

    Update WidgetForm.php

--- modules/hostgroupstatus/includes/WidgetForm.php
+++ modules/hostgroupstatus/includes/WidgetForm.php
@@ -4,20 +4,21 @@ namespace Modules\HostGroupStatus\Includes;
 
 use Zabbix\Widgets\{
 	CWidgetField,
-	CWidgetForm,
-	Fields\CWidgetFieldCheckBox,
-	Fields\CWidgetFieldColor,
-	Fields\CWidgetFieldIntegerBox,
-	Fields\CWidgetFieldMultiSelectGroup,
-	Fields\CWidgetFieldMultiSelectHost,
-	Fields\CWidgetFieldRadioButtonList,
-	Fields\CWidgetFieldTags, // <-- ADICIONADO
-	Fields\CWidgetFieldTextBox
+	CWidgetForm
 };
 
-/**
- * Host Group Status widget form.
- */
+use Zabbix\Widgets\Fields\{
+	CWidgetFieldCheckBox,
+	CWidgetFieldColor,
+	CWidgetFieldIntegerBox,
+	CWidgetFieldMultiSelectGroup,
+	CWidgetFieldMultiSelectHost,
+	CWidgetFieldRadioButtonList,
+	CWidgetFieldSeverities, // <-- Importante
+	CWidgetFieldTags, 
+	CWidgetFieldTextBox
+};
+
 class WidgetForm extends CWidgetForm {
 
 	// Count modes
@@ -25,58 +26,70 @@ class WidgetForm extends CWidgetForm {
 	public const COUNT_MODE_WITHOUT_ALARMS = 2;
 	public const COUNT_MODE_ALL = 3;
 	
-	// Severities (copiado do outro widget)
-	public const SEVERITY_NOT_CLASSIFIED = 0;
-	public const SEVERITY_INFORMATION = 1;
-	public const SEVERITY_WARNING = 2;
-	public const SEVERITY_AVERAGE = 3;
-	public const SEVERITY_HIGH = 4;
-	public const SEVERITY_DISASTER = 5;
-
 	public function addFields(): self {
 		// Host group selection
 		$this->addField(
 			(new CWidgetFieldMultiSelectGroup('hostgroups', _('Host groups')))
 				->setFlags(CWidgetField::FLAG_NOT_EMPTY | CWidgetField::FLAG_LABEL_ASTERISK)
-		)
+		);
+
 		// Host selection
-		->addField(
+		$this->addField(
 			(new CWidgetFieldMultiSelectHost('hosts', _('Hosts')))
 				->setFlags(CWidgetField::FLAG_LABEL_ASTERISK)
-		)
+		);
 		
-		// ----- NOVOS CAMPOS ADICIONADOS -----
-		->addField(
+		// Exclude Hosts
+		$this->addField(
 			(new CWidgetFieldMultiSelectHost('exclude_hosts', _('Exclude Hosts')))
-		)
-		->addField(
+		);
+
+		// --- MUDANÇA: SEVERIDADE ---
+		// Substitui os 6 checkboxes individuais por um único campo de Severities
+		$this->addField(
+			(new CWidgetFieldSeverities('severities', _('Severity')))
+				->setDefault([
+					TRIGGER_SEVERITY_NOT_CLASSIFIED,
+					TRIGGER_SEVERITY_INFORMATION,
+					TRIGGER_SEVERITY_WARNING,
+					TRIGGER_SEVERITY_AVERAGE,
+					TRIGGER_SEVERITY_HIGH,
+					TRIGGER_SEVERITY_DISASTER
+				])
+		);
+		// --- FIM DA MUDANÇA ---
+
+		// Problem tags
+		$this->addField(
 			(new CWidgetFieldRadioButtonList('evaltype', _('Problem tags'), [
 				TAG_EVAL_TYPE_AND_OR => _('And/Or'),
 				TAG_EVAL_TYPE_OR => _('Or')
 			]))
 				->setDefault(TAG_EVAL_TYPE_AND_OR)
-		)
-		->addField(
+		);
+
+		$this->addField(
 			new CWidgetFieldTags('tags')
-		)
-		->addField(
+		);
+
+		// Filters
+		$this->addField(
 			(new CWidgetFieldCheckBox('show_acknowledged', _('Show acknowledged problems')))
 				->setDefault(1)
-		)
-		->addField(
+		);
+
+		$this->addField(
 			(new CWidgetFieldCheckBox('show_suppressed', _('Show suppressed problems')))
 				->setDefault(0)
-		)
-		// --- MUDANÇA AQUI: Adicionado filtro de Manutenção ---
-		->addField(
+		);
+
+		$this->addField(
 			(new CWidgetFieldCheckBox('exclude_maintenance', _('Exclude hosts in maintenance')))
 				->setDefault(0)
-		)
-		// --- FIM DA MUDANÇA ---
-		// ----- FIM DOS NOVOS CAMPOS -----
+		);
 
-		// Count mode selection
-		->addField(
+		// Count mode
+		$this->addField(
 			(new CWidgetFieldRadioButtonList('count_mode', _('Count Mode'), [
 				self::COUNT_MODE_WITH_ALARMS => _('Hosts with alarms'),
 				self::COUNT_MODE_WITHOUT_ALARMS => _('Hosts without alarms'),
@@ -84,85 +97,68 @@ class WidgetForm extends CWidgetForm {
 			]))
 				->setDefault(self::COUNT_MODE_WITH_ALARMS)
 				->setFlags(CWidgetField::FLAG_NOT_EMPTY)
-		)
-		// Widget color selection
-		->addField(
+		);
+
+		// Widget color
+		$this->addField(
 			(new CWidgetFieldColor('widget_color', _('Widget Color')))
 				->setDefault('4CAF50')
 				->setFlags(CWidgetField::FLAG_NOT_EMPTY)
-		)
-
-		// ----- NOVOS FILTROS DE SEVERIDADE -----
-		->addField(
-			(new CWidgetFieldCheckBox('show_not_classified', _('Show Not classified')))
-				->setDefault(1)
-		)
-		->addField(
-			(new CWidgetFieldCheckBox('show_information', _('Show Information')))
-				->setDefault(1)
-		)
-		->addField(
-			(new CWidgetFieldCheckBox('show_warning', _('Show Warning')))
-				->setDefault(1)
-		)
-		->addField(
-			(new CWidgetFieldCheckBox('show_average', _('Show Average')))
-				->setDefault(1)
-		)
-		->addField(
-			(new CWidgetFieldCheckBox('show_high', _('Show High')))
-				->setDefault(1)
-		)
-		->addField(
-			(new CWidgetFieldCheckBox('show_disaster', _('Show Disaster')))
-				->setDefault(1)
-		)
-		// ----- FIM DOS FILTROS DE SEVERIDADE -----
+		);
 
 		// Show group name
-		->addField(
+		$this->addField(
 			(new CWidgetFieldCheckBox('show_group_name', _('Show group name')))
 				->setDefault(1)
-		)
+		);
+
 		// Group name text
-		->addField(
+		$this->addField(
 			(new CWidgetFieldTextBox('group_name_text', _('Custom group name')))
 				->setDefault('')
-		)
+		);
+
 		// URL redirection settings
-		->addField(
+		$this->addField(
 			(new CWidgetFieldCheckBox('enable_url_redirect', _('Enable URL redirect')))
 				->setDefault(0)
-		)
-		->addField(
+		);
+
+		$this->addField(
 			(new CWidgetFieldTextBox('redirect_url', _('Redirect URL')))
 				->setDefault('')
-		)
-		->addField(
+		);
+
+		$this->addField(
 			(new CWidgetFieldCheckBox('open_in_new_tab', _('Open in new tab')))
 				->setDefault(1)
-		)
+		);
+
 		// Font settings
-		->addField(
+		$this->addField(
 			(new CWidgetFieldIntegerBox('font_size', _('Font Size (px)')))
 				->setDefault(14)
 				->setFlags(CWidgetField::FLAG_NOT_EMPTY)
-		)
-		->addField(
+		);
+
+		$this->addField(
 			(new CWidgetFieldTextBox('font_family', _('Font Family')))
 				->setDefault('Arial, sans-serif')
-		)
+		);
+
 		// Border settings
-		->addField(
+		$this->addField(
 			(new CWidgetFieldCheckBox('show_border', _('Show Border')))
 				->setDefault(1)
-		)
-		->addField(
+		);
+
+		$this->addField(
 			(new CWidgetFieldIntegerBox('border_width', _('Border Width (px)')))
 				->setDefault(2)
-		)
+		);
+
 		// Padding
-		->addField(
+		$this->addField(
 			(new CWidgetFieldIntegerBox('padding', _('Padding (px)')))
 				->setDefault(10)
 				->setFlags(CWidgetField::FLAG_NOT_EMPTY)
