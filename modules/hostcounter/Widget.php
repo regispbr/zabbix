776cb1e6f8fed7caa53e95c16dbd614c5eb33ae0
regispbr <reginaldo_uni9@hotmail.com>
2025-12-04 23:39:10 -0300

    Update Widget.php

--- modules/hostcounter/Widget.php
+++ modules/hostcounter/Widget.php
@@ -12,13 +12,8 @@ class Widget extends CWidget {
 
 	public function getAssets(): array {
 		return [
-			// Arquivos para o Dashboard
 			'js' => ['class.widget.js'],
-			
-			// A chave 'css' DEVE existir, mas pode ser vazia
 			'css' => ['style.css'],
-			
-			// Arquivo para a tela de Edição
 			'edit_js' => ['widget.edit.js']
 		];
 	}
@@ -27,15 +22,6 @@ class Widget extends CWidget {
 		return [
 			'class.widget.js' => [
 				'Host Counter' => _('Host Counter'),
-				'No host group selected' => _('No host group selected'),
-				'hosts' => _('hosts'),
-				'host' => _('host'),
-				'Count Problems' => _('Count Problems'),
-				'Count Items' => _('Count Items'),
-				'Count Triggers' => _('Count Triggers'),
-				'Count Disabled Hosts' => _('Count Disabled Hosts'),
-				'Count Maintenance Hosts' => _('Count Maintenance Hosts'),
-				'Custom Icon' => _('Custom Icon'),
 				'Total Hosts' => _('Total Hosts'),
 				'Active Hosts' => _('Active Hosts'),
 				'Disabled Hosts' => _('Disabled Hosts'),
