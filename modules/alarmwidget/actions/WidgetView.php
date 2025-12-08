460841e29e93e19dd5bd71484f5c1345aa5e6fc2
Dutervas <149430977+Dutervas@users.noreply.github.com>
2025-12-08 16:26:23 -0300

    Update WidgetView.php

--- modules/alarmwidget/actions/WidgetView.php
+++ modules/alarmwidget/actions/WidgetView.php
@@ -79,7 +79,7 @@ class WidgetView extends CControllerDashboardWidgetView {
 			'show_symptoms' => false,
 			'show_suppressed' => $engine_show_suppressed,
 			'acknowledgement_status' => $ack_status,
-			'show_opdata' => 0
+			'show_opdata' => 1
 		], $search_limit);
 
 		// Coleta de IDs
